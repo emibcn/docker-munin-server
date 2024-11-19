0528c5b21045c0bd7f2f811606acfae45554e81e
Emilio del Giorgio <emilio.delgiorgio@es.abb.com>
2024-11-19 17:21:56 +0100

    Allow easy data path setting

--- create-initial.sh
+++ create-initial.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+INIT_PATH="."
+REAL_PATH="$(realpath "${INIT_PATH}")"
+
 mkdir -vp \
     cache \
     lib \
     log \
     run
 
-echo "LOCAL=${PWD}" > .env
+echo "LOCAL=${REAL_PATH}" > .env
