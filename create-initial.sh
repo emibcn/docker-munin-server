e5d0d460604faad9ff4bd73b0e7a79f8341b19aa
Emilio del Giorgio <emilio.delgiorgio@es.abb.com>
2024-11-19 17:23:04 +0100

    Allow easy data path setting - v2

--- create-initial.sh
+++ create-initial.sh
@@ -5,9 +5,9 @@ INIT_PATH="."
 REAL_PATH="$(realpath "${INIT_PATH}")"
 
 mkdir -vp \
-    cache \
-    lib \
-    log \
-    run
+    "${REAL_PATH}/cache" \
+    "${REAL_PATH}/lib" \
+    "${REAL_PATH}/log" \
+    "${REAL_PATH}/run"
 
 echo "LOCAL=${REAL_PATH}" > .env
